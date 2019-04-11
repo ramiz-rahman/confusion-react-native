91f5d67f98e0aa0b9c3b24626764548539e2df9b
ramiz-rahman <ramiz.rahman@outlook.com>
2019-04-11 06:50:38 +0600

    Activity Indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './loadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -29,13 +30,24 @@ class Menu extends Component {
     };
 
     const { navigate } = this.props.navigation;
-    return (
-      <FlatList
-        data={this.props.dishes.dishes}
-        renderItem={renderMenuItem}
-        keyExtractor={item => item.id.toString()}
-      />
-    );
+
+    if (this.props.dishes.isLoading) {
+      return <Loading />;
+    } else if (this.props.dishes.errMess) {
+      return (
+        <View>
+          <Text>{this.props.dishes.errMess}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.dishes.dishes}
+          renderItem={renderMenuItem}
+          keyExtractor={item => item.id.toString()}
+        />
+      );
+    }
   }
 }
 
